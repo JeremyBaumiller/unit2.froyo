ba2c1dfd24cadebde702ecb61cd8a82bd44dcb7b
JeremyBaumiller <jbaumil07@gmail.com>
2024-03-16 20:58:23 -0400

    working box

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const greeting = prompt("choose a flavor of froyo separated by commas");
-
-const froyo = [
-  { name: "chocolate" },
-  { name: "strawberry" },
-  { name: "coffee" },
-];
-let flavors = "";
-for (const flavors of froyo) {
-  if (froyo[flavors]) {
-    froyo[flavors]++;
-  } else {
-    froyo[flavors] = 1;
+console.log(greeting);
+let order = greeting.split(", ");
+console.log(order);
+function flavor() {
+  const froyo = {};
+  // { name: "chocolate" },
+  // { name: "strawberry" },
+  // { name: "coffee" },
+  for (const flavor of order) {
+    if (froyo[flavor]) {
+      froyo[flavor] += 1;
+    } else {
+      froyo[flavor] = 1;
+    }
   }
+  return froyo;
 }
-console.table(froyo);
+console.table(flavor(order));
